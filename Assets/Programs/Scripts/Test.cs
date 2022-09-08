d8626dc34ad268df5c6983dd4303fb28187a868e
86K <577635188@qq.com>
2022-09-08 16:09:51 +0800

    init

---
+++ Assets/Programs/Scripts/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private Button btn;
+
+    private void Awake()
+    {
+        btn.onClick.AddListener(() => Debug.Log($"123"));
+    }
+}
